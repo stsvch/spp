641306760032dbf95d7651c71362f68447cb2dd5
stasvch <yana08469@gmail.com>
2025-10-16 14:08:11 +0300

    lab1

---
+++ app/src/pages/Projects.jsx
@@ -0,0 +1,8 @@
+export default function Projects() {
+  return (
+    <section>
+      <h1>Проекты</h1>
+      <p>Список будет позже</p>
+    </section>
+  );
+}
---
+++ app/src/utils/id.js
@@ -0,0 +1,4 @@
+export function generateId() {
+  if (typeof crypto !== "undefined" && crypto.randomUUID) return crypto.randomUUID();
+  return "id-" + Date.now().toString(36) + "-" + Math.random().toString(36).slice(2, 8);
+}
